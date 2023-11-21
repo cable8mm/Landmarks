603d0f68aada909a7aa0b7455216b9dad6a5c197
Sam Lee <cable8mm@gmail.com>
2023-11-21 17:13:25 +1300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by samgu.lee on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
